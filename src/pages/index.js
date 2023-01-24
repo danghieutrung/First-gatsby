9b0c362482cdbe7c22a63ee4183a1a9777ed0462
Dang Trung Hieu <hieudangtrung@protonmail.com>
2023-01-24 17:56:24 +0800

    Finished Gatsby Tutorial p3

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,20 @@
 import React from "react"
 import Layout from "../components/layout"
+import { StaticImage } from "gatsby-plugin-image"
 
 const IndexPage = () => {
   return (
-    <Layout pageTitle="Home Page"><p>lol</p></Layout>
+    <Layout pageTitle="Home Page">
+      <p>lol</p>
+      <StaticImage
+        alt="Clifford, a reddish-brown pitbull, posing on a couch and looking stoically at the camera"
+        src="../images/taytay.1000x1000x1-354485103"
+      />
+      <h1>Mangooo</h1>
+    </Layout>
   )
 }
+
+export const Head = () => <title>Home Page</title>
 
 export default IndexPage
